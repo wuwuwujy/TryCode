03cddc6ce956a5552a936c5c3bc4e646beb20ea4
Yilun Zhao <zyilun@umich.edu>
2019-12-15 18:22:19 -0600

    Add files via upload

--- super_resolution/srgan/srgan_loss.py
+++ super_resolution/srgan/srgan_loss.py
@@ -27,13 +27,13 @@ class TVLoss(nn.Module):
 class GeneratorLoss(nn.Module):
     def __init__(self):
         super(GeneratorLoss, self).__init__()
-        vgg = vgg16(pretrained=True)
-        loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
+        vgg_16 = vgg16(pretrained=True)
+        loss_network = nn.Sequential(*list(vgg_16.features)[:31]).eval()
         for param in loss_network.parameters():
             param.requires_grad = False
         self.loss_network = loss_network
         self.mse_loss = nn.MSELoss()
-        self.tv_loss = TVLoss()
+        self.TV_loss = TVLoss()
 
     def forward(self, out_labels, out_images, target_images):
         # Adversarial Loss
@@ -43,8 +43,9 @@ class GeneratorLoss(nn.Module):
         # Image Loss
         image_loss = self.mse_loss(out_images, target_images)
         # TV Loss
-        tv_loss = self.tv_loss(out_images)
-        return image_loss + 0.001 * adversarial_loss + 0.006 * perception_loss + 2e-8 * tv_loss
+        tv_loss = self.TV_loss(out_images)
+        loss = 0.001 * adversarial_loss + 0.006 * perception_loss + image_loss + 2e-8 * tv_loss
+        return loss
 
 
 if __name__ == "__main__":
